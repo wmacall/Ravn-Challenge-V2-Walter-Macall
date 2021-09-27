2e1d1dc479b9657a16df357f5c6d473b75c1d988
wmacall15 <wmacall@applaudostudios.com>
2021-09-27 12:37:53 -0600

    Added style file for separator component

---
+++ src/components/Separator/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {COLORS} from '@assets';
+import {size} from '@constants';
+
+export default StyleSheet.create({
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: COLORS.BLACK_OPACITY_01,
+    marginLeft: size.m,
+  },
+});
