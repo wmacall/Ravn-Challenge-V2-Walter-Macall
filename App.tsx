db1daf4be161d8da4d60cca80eb48c0c910ce832
wmacall15 <wmacall@applaudostudios.com>
2021-09-28 09:02:49 -0600

    Added app iconsand implemented splashscreen on Android

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppRoutes} from '@routes';
 import {ApolloProvider} from '@apollo/client';
+import RNBootSplash from 'react-native-bootsplash';
 import {client} from 'graphql/config';
 
 const App = () => (
   <ApolloProvider client={client}>
-    <NavigationContainer>
+    <NavigationContainer onReady={() => RNBootSplash.hide({fade: true})}>
       <AppRoutes />
     </NavigationContainer>
   </ApolloProvider>
